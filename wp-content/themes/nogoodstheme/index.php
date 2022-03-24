e85db27e7a62bd2972c904f6882f29d66346b4d8
Vic <syuan0212@gmail.com>
2022-03-24 13:25:11 +0800

    fix image src and html

--- wp-content/themes/nogoodstheme/index.php
+++ wp-content/themes/nogoodstheme/index.php
@@ -1,19 +1,34 @@
-<?php get_header(); ?>
-<main class="pt-5">
-  <section>
-    <?php get_template_part('company'); ?>
-  </section>
-  <section class="my-5 py-5 bg-light">
-    <div>
-      <?php get_template_part('about'); ?>
-    </div>
-  </section>
-  <section class="mb-5">
-    <?php get_template_part('assess'); ?>
-  </section>
-  <section class="bg-light">
-    <?php get_template_part('join'); ?>
-  </section>
-</main>
-<?php get_footer(); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>梅天涼 NoGood 全新開幕</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/style.css">
+  </head>
+  <body>
+    <?php get_header(); ?>
+    <main class="pt-5">
+      <section class="container">
+        <?php get_template_part('company'); ?>
+      </section>
+      <section class="container my-5 py-5 bg-light">
+        <div>
+          <?php get_template_part('about'); ?>
+        </div>
+      </section>
+      <section class="container mb-5">
+        <?php get_template_part('assess'); ?>
+      </section>
+      <section class="bg-light">
+        <?php get_template_part('join'); ?>
+      </section>
+    </main>
+    <?php get_footer(); ?>
+  </body>
+</html>
+
+
   
